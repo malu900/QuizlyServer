0443fa7997f2e4cbfe32b90ebe9ab89f9ef2e46b
Zheng-Rong Cai <ZCai319@gmail.com>
2020-10-06 09:55:06 +0200

    project kinda opgezet? controleer pls

---
+++ src/test/java/com/example/quizly/QuizlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
